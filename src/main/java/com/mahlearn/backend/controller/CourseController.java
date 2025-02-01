52ba3d5dd5950377e2b286d648b2075feedf6db8
Eduardo Neira <edun327@gmail.com>
2025-02-01 13:15:59 +1100

    Course CORS

--- src/main/java/com/mahlearn/backend/controller/CourseController.java
+++ src/main/java/com/mahlearn/backend/controller/CourseController.java
@@ -2,14 +2,17 @@ package com.mahlearn.backend.controller;
 
 import com.mahlearn.backend.model.Course;
 import com.mahlearn.backend.service.CourseService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/courses")
+@CrossOrigin
 public class CourseController {
 
     private final CourseService courseService;
